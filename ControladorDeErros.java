923152e7ae5372a0c7a7a219a4d90eab229acc46
lucas lopes <lucas.clopes04@gmail.com>
2024-09-24 08:32:53 -0300

    ErrorController

---
+++ ControladorDeErros.java
@@ -0,0 +1,64 @@
+public class ControladorDeErros implements Cloneable
+{
+    private int qtdMax, qtdErr=0;
+
+    public ControladorDeErros (int qtdMax) throws Exception
+    {
+        // verifica se qtdMax fornecida não é positiva, lançando
+        // uma exceção.
+        // armazena qtdMax fornecida em this.qtdMax.
+        if (qtdMax<0) throw new Exception ("Quantidade invalida");
+
+        this.qtdMax=qtdMax;
+    }
+
+    public void registreUmErro () throws Exception
+    {
+        // verifica se this.qtdErr ja é igual a this.qtdMax,
+        // lançando excecao em caso positivo ou
+        // incrementando this.qtdErr em caso negativo
+        if (this.qtdErr==this.qtdMax) throw new Exception ("Errou alem do maximo");
+
+        this.qtdErr++;
+    }
+
+    public boolean isAtingidoMaximoDeErros  ()
+    {
+        // returna true se this.qtdErr for igual a this.qtdMax,
+        // ou então false, caso contrario.
+        /*
+        if (this.qtdErr==this.qtdMax)
+            return true;
+        else
+            return false;
+        */
+
+        return this.qtdErr==this.qtdMax;
+    }
+
+    public String toString ()
+    {
+        return this.qtdErr + "/" + this.qtdMax;
+    }
+
+    public boolean equals (Object obj)
+    {
+        // verificar se this e obj possuem o mesmo conteúdo, retornando
+        // true no caso afirmativo ou false no caso negativo
+    }
+
+    public int hashCode ()
+    {
+        // calcular e retornar o hashcode de this
+    }
+
+    public ControladorDeErros (ControladorDeErros c) throws Exception // construtor de cópia
+    {
+        // copiar c.qtdMax e c.qtdErr, respectivamente em, this.qtdMax e this.qtdErr
+    }
+
+    public Object clone ()
+    {
+        // returnar uma cópia de this
+    }
+}
