5b698c41ef6faa557671c0ebefdace2852c791f9
Lucas Lopes <lucas.clopes04@gmail.com>
2024-09-25 10:12:40 -0300

    Principal Tracinhos(concluido)

--- Tracinhos.java
+++ Tracinhos.java
@@ -1,58 +1,101 @@
-/*public class Tracinhos implements Cloneable
+package Forca;
+
+public class Tracinhos implements Cloneable
 {
-    private char texto [];
+    private char[] texto;
 
-    public Tracinhos (int qtd) throws Exception
-    {
-		// verifica se qtd n�o � positiva, lan�ando uma exce��o.
-		// instancia this.texto com um vetor com tamanho igual qtd.
-		// preenche this.texto com underlines (_).
+    public Tracinhos (int qtd) throws Exception {
+        if (qtd < 0) {
+            throw new Exception("Error: Qtd nao é positiva!");
+        }
+        this.texto = new char[qtd];
+        for(int i=0;i<qtd;i++){
+            this.texto[i]='_';
+        }
+        System.out.println(this.texto);
     }
 
-    public void revele (int posicao, char letra) throws Exception
-    {
-		// verifica se posicao � negativa ou ent�o igual ou maior
-		// do que this.texto.length, lan�ando uma exce��o.
-		// armazena a letra fornecida na posicao tambem fornecida
-		// do vetor this.texto
+    public void revele (int posicao, char letra) throws Exception {
+        if(posicao<0 || posicao>this.texto.length){
+            throw new Exception("Error: Posicao fora do limite!");
+        }
+        try {
+            this.texto[posicao - 1] = letra;
+        }catch (Exception err){
+            throw new Exception("Error: Posicao fora do limite!");
+        }
+        System.out.println(this.texto);
     }
 
-    public boolean isAindaComTracinhos ()
-    {
-        // percorre o vetor de char this.texto e verifica
-        // se o mesmo ainda contem algum underline ou se ja
-        // foram todos substituidos por letras; retornar true
-        // caso ainda reste algum underline, ou false caso
-        // contrario
+    public boolean isAindaComTracinhos () {
+        for (int i = 0; i < this.texto.length; i++) {
+            if (this.texto[i] == '_') {
+                return true;
+            }
+        }
+        return false;
     }
+    @Override
+    public String toString () {
+        StringBuilder resultado = new StringBuilder();
 
-    public String toString ()
-    {
-        // retorna um String com TODOS os caracteres que h�
-        // no vetor this.texto, intercalados com espa�os em
-        // branco
+        for (int i = 0; i < this.texto.length; i++) {
+            resultado.append(this.texto[i]); // Adiciona o caractere
+            if (i < this.texto.length - 1) {
+                resultado.append(" "); // Adiciona um espaço se não for o último
+            }
+        }
+        return resultado.toString();
     }
-
+    @Override
     public boolean equals (Object obj)
     {
-        // verificar se this e obj possuem o mesmo conte�do, retornando
-        // true no caso afirmativo ou false no caso negativo
-    }
+        if(this.texto == obj) return true;
 
+        if (obj ==  null) return false;
+
+        if (obj.getClass()!=this.getClass()) return false;
+
+        Tracinhos tra = (Tracinhos) obj;
+
+        if (this.texto.length != tra.texto.length) return false;
+
+        for (int i = 0; i < this.texto.length; i++) {
+            if(this.texto[i]!=tra.texto[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
     public int hashCode ()
     {
+        int varRand = 1;
+        if (this.texto!=null){
+            for (int i = 0; i < this.texto.length ; i++) {
+                varRand = varRand * 11 + this.texto[i];
+            }
+        }
+        if (varRand<0) varRand=-varRand;
+        return varRand;
         // calcular e retornar o hashcode de this
     }
 
     public Tracinhos (Tracinhos t) throws Exception // construtor de c�pia
     {
-        // intanciar this.texto um vetor com o mesmo tamanho de t.texto
-        // e copilar o conte�do de t.texto para this.texto
+        this.texto = new char[t.texto.length];
+
+        for (int i = 0; i < t.texto.length; i++) {
+            this.texto[i] = t.texto[i];
+        }
     }
 
     public Object clone ()
     {
-        // retornar uma copia de this
+        Tracinhos t = null;
+        try{
+            t = new Tracinhos(this);
+        }catch (Exception ignored){}
+        return t;
     }
 }
-*/
